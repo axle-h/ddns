f5bc256aa413cff5f89eefe6a6a8ea6712b257b3
Alex Haslehurst <alex.haslehurst@gmail.com>
2024-06-14 23:33:38 +0100

    ddns

--- src/dns/porkbun/client.ts
+++ src/dns/porkbun/client.ts
@@ -8,26 +8,65 @@ export class PorkbunClient {
         private readonly baseAddress: string = 'https://porkbun.com') {
     }
 
-    async listDomains() {
-        const response = await fetch(
-            `${this.baseAddress}/api/json/v3/domain/listAll`,
-            { body: JSON.stringify(this.auth()), method: 'POST' }
-        )
-        console.log(await response.json())
+    async listDomains(): Promise<Domain[]> {
+        const response = await this.request<ListDomainsResponse>('/api/json/v3/domain/listAll')
+        return response.domains
     }
 
-    async getDnsRecords(domain: string) {
-        const response = await fetch(
-            `${this.baseAddress}/api/json/v3/dns/retrieve/ax-h.com`,
-            { body: JSON.stringify(this.auth()), method: 'POST' }
-        )
-        console.log(await response.json())
+    async listDnsRecords(domain: string): Promise<DnsRecord[]> {
+        const response = await this.request<ListDnsRecordsResponse>(`/api/json/v3/dns/retrieve/${domain}`)
+        return response.records
     }
 
-    private auth() {
-        return {
-            secretapikey: this.secretKey,
-            apikey: this.apikey
+    async updateDnsRecord(domain: string, record: DnsRecord): Promise<void> {
+        await this.request(
+            `/api/json/v3/dns/edit/${domain}/${record.id}`,
+            {
+                // this api does not support a FQ domain
+                name: record.name.replace(new RegExp(`\.${domain}$`), ''),
+                type: record.type,
+                content: record.content
+            }
+        )
+    }
+
+    private async request<R extends PorkbunResponse>(url: string, request: any = {}): Promise<R> {
+        const response = await fetch(this.baseAddress + url, {
+            body: JSON.stringify({
+                ...request,
+                secretapikey: this.secretKey,
+                apikey: this.apikey
+            }),
+            method: 'POST'
+        })
+        const body: R = await response.json()
+        if (body.status !== 'SUCCESS') {
+            throw new Error(`porkbun api response ${body.status} ${body.message}`)
         }
+        return body
     }
+}
+
+interface PorkbunResponse {
+    status: 'SUCCESS' | 'ERROR'
+    message?: string
+}
+
+export interface Domain {
+    domain: string
+}
+
+interface ListDomainsResponse extends PorkbunResponse {
+    domains: Domain[]
+}
+
+export interface DnsRecord {
+    id: string // '12345'
+    name: string // 'home.example.com'
+    type: string // 'A'
+    content: string // '8.8.8.8'
+}
+
+interface ListDnsRecordsResponse extends PorkbunResponse {
+    records: DnsRecord[]
 }
--- src/dns/porkbun/provider.ts
+++ src/dns/porkbun/provider.ts
@@ -1,14 +1,50 @@
 import {DnsProvider} from "../provider";
-import {PorkbunClient} from "./client";
+import {DnsRecord, PorkbunClient} from "./client";
 
 export class PorkbunDnsProvider implements DnsProvider {
     constructor(private readonly client: PorkbunClient) {}
 
+    private dnsRecords: Promise<(DnsRecord & { domain: string })[]> | null = null
+
     async setDnsRecord(domain: string, wanAddress: string): Promise<void> {
-        // 1. list domains
-        // 2. get the specific dns record
-        // 4. update it if required
-        await this.client.listDomains()
+        if (this.dnsRecords === null) {
+            this.dnsRecords = this.getDnsRecords()
+        }
+
+        const dnsRecords = await this.dnsRecords
+        const record = dnsRecords.find(record =>
+            record.name === domain && record.type === 'A')
+
+        if (!record) {
+            console.error(`cannot find domain ${domain}`)
+            return
+        }
+
+        if (record.content === wanAddress) {
+            console.info(`domain ${domain} does not need updating`)
+            return
+        }
+
+        console.info(`updating domain ${domain} ${record.content} -> ${wanAddress}`)
+
+        await this.client.updateDnsRecord(record.domain, {
+            ...record,
+            content: wanAddress
+        })
+
+        record.content = wanAddress
+    }
+
+    private async getDnsRecords() {
+        const domains = await this.client.listDomains()
+        const domainRecords = await Promise.all(
+            domains.map(async domain => ({
+                domain: domain.domain,
+                dns: await this.client.listDnsRecords(domain.domain)
+            }))
+        )
+        return domainRecords.flatMap(({domain, dns}) =>
+            dns.map(record => ({...record, domain})))
     }
 
 }
